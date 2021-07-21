ea6567294c12e2ef88c4ba1bb29bc19430037d7b
xzartsust <68950796+xzartsust@users.noreply.github.com>
2021-07-21 18:58:04 +0300

    fix bug in main.py

--- main.py
+++ main.py
@@ -15,6 +15,7 @@ bot = Bot(command_prefix = '_', help_command = None, intents = intents)
 bot.load_extension('cogs.commands.send_role_embed')
 bot.load_extension('cogs.event.add_role')
 bot.load_extension('cogs.event.remove_role')
+bot.load_extension('cogs.channel.create_channel')
 
 
 token = os.environ.get('TOKEN')
