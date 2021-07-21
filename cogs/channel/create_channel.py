2d3196394f38a724cb83c4c7a278625d56090635
xzartsust <68950796+xzartsust@users.noreply.github.com>
2021-07-21 19:23:16 +0300

    fix bug in main.py

--- cogs/channel/create_channel.py
+++ cogs/channel/create_channel.py
@@ -1,9 +1,9 @@
 import logging
+from operator import le
 
 import config
 import discord
 from discord.ext import commands
-from discord.ext.commands.context import Context
 from discord.utils import get
 
 
@@ -11,22 +11,34 @@ class PrivateChannel(commands.Cog):
     def __init__(self, bot: commands.Bot) -> None:
         super().__init__()
         self.bot = bot
+        self.all_channel = []
 
     @commands.Cog.listener()
     async def on_voice_state_update(self, member: discord.Member, before: discord.VoiceState, after: discord.VoiceState):
         
-        print(f'\n{member}\n\n{before}\n\n{after}\n')
+        print(f'Before: {before}\n')
+        print(f'After: {after}\n')
+
+        '''
+        category = int(config.category_id)
 
         if member.voice.channel is not None and member.voice.channel.id == int(config.voice_id):
             logging.info('Member connect in channel {0}'.format(after.channel.name))
+            
+            try:
+                category_main: discord.CategoryChannel = get(member.guild.categories, id = category)
+                channel: discord.VoiceChannel = await member.guild.create_voice_channel(name = f'Приватный ({member.display_name})', category = category_main)
+                
+                await channel.set_permissions(member, connect = True, mute_members = True, move_members = True, manage_channels = True, manage_roles = True)
+                await member.move_to(channel)
+
+                self.all_channel.append(int(channel.id))
+
+            except Exception as e:
+                logging.exception(e)
         
-        '''
-        try:
-            if after.channel is not None and member.voice.channel.id == int(config.voice_id)
-
-        except Exception as e:
-            logging.exception(e)
-        '''
-
+        if after.channel.id in self.all_channel and len(get(member.guild.voice_channel, id = after.channel.id).members) == 0:
+            pass
+            '''
 def setup(bot: commands.Bot):
     bot.add_cog(PrivateChannel(bot))
