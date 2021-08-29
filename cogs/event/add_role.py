c08513c4d8212aa4531a96b34301828cc6bd908e
xzartsust <68950796+xzartsust@users.noreply.github.com>
2021-08-29 21:12:35 +0300

    Update add_role.py

--- cogs/event/add_role.py
+++ cogs/event/add_role.py
@@ -2,18 +2,40 @@ import logging
 
 import config
 import discord
+import psycopg2
 from discord import utils
 from discord.ext import commands
 
 class GiveRole(commands.Cog):
-    def __init__(self, bot: commands.Bot) -> None:
+    username = config.USER
+    host = config.HOST
+    database = config.DATABASE
+    password = config.PASSWORD
+    port = 5432
+
+    def __init__(self, bot: commands.Bot):
         super().__init__()
         self.bot = bot
+        self.conn = psycopg2.connect(
+            user = self.username,
+            host = self.host,
+            database = self.database,
+            password = self.password,
+            port = self.port
+        )
 
     @commands.Cog.listener()
     async def on_raw_reaction_add(self, payload: discord.RawReactionActionEvent):
 
         if not payload.member.bot:
+
+            cursor = self.conn.cursor()
+
+            cursor.execute(f'SELECT message_id FROM public."general" WHERE channel_id = {payload.channel_id};')
+            channel_id = cursor.fetchone()
+            self.conn.commit()
+            print(channel_id)
+
             channel: discord.TextChannel = self.bot.get_channel(int(config.channel))
             message = await channel.fetch_message(int(config.message))
             member: discord.Member = utils.get(message.guild.members, id = payload.user_id)
@@ -47,3 +69,4 @@ class GiveRole(commands.Cog):
 '''
 def setup(bot: commands.Bot):
     bot.add_cog(GiveRole(bot))
+    print(bot)
