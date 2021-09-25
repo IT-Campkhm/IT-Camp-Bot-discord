71e8772d5c2f80b171d8265c3aac87f893c244ce
xzartsust <68950796+xzartsust@users.noreply.github.com>
2021-09-25 23:50:59 +0300

    Fix bug

--- owner_func/test.py
+++ owner_func/test.py
@@ -9,25 +9,26 @@ class TestOwner(commands.Cog):
     def __init__(self, bot: commands.Bot):
         super().__init__()
         self.bot = bot
-        self.channel_rules = self.bot.get_channel(CHANNEL_RULES)
-        self.channel_greeting = self.bot.get_channel(CHANNEL_GREETING)
-        self.cnannel_roles = self.bot.get_channel(CNANNEL_ROLES)
 
     @commands.command(name = 'test')
     @commands.is_owner()
     async def _test_owner(self, ctx: Context):
         try:
+            channel_rules = self.bot.get_channel(CHANNEL_RULES)
+            channel_greeting = self.bot.get_channel(CHANNEL_GREETING)
+            cnannel_roles = self.bot.get_channel(CNANNEL_ROLES)
+
             emb = (discord.Embed(
                 title = f'Вітаємо вас на сервері проекту {ctx.guild.name}!',
                 description = 'Головна мета IT-camp це зробити комфортне місце для вивчення різних напрямів ІТ-технологій в професійному середовищі з неймовірною атмосферою та крутою командою💥\n' \
-                            f'Ознайометесь з правилами проекту в каналі {self.channel_rules.mention}.\n'
-                            f'Також можете вибрати роль в каналі {self.cnannel_roles.mention}, яка найбільше підходить до вашого виду діяльності.',
+                            f'Ознайометесь з правилами проекту в каналі {channel_rules.mention}.\n'
+                            f'Також можете вибрати роль в каналі {cnannel_roles.mention}, яка найбільше підходить до вашого виду діяльності.',
                 timestamp = ctx.message.created_at
             ).set_footer(
                 text = f'{ctx.author.id} | Приємно провести час на нашому проекті',
                 icon_url = ctx.author.icon_url
             ))
-            await self.channel_greeting.send(f'{ctx.author.mention}', embed = emb)
+            await channel_greeting.send(f'{ctx.author.mention}', embed = emb)
         except Exception as e:
             logging.exception(e)
 
