b5a2bcff633e54583845037a65bac027a9ec0c1f
xzartsust <68950796+xzartsust@users.noreply.github.com>
2021-09-25 23:46:43 +0300

    Greeeting func
    Добавлено функцію для привітання нових користувачів на сервері

--- main.py
+++ main.py
@@ -3,13 +3,12 @@ from discord.ext.commands import Bot
 import logging
 import os
 
-logging.basicConfig(format = u'%(filename)s [LINE:%(lineno)d] #%(levelname)-8s [%(asctime)s]  %(message)s',
-                    level = logging.INFO)
+logging.basicConfig(format=u'%(filename)s [LINE:%(lineno)d] #%(levelname)-8s [%(asctime)s]  %(message)s',
+                    level=logging.INFO)
 
 intents = discord.Intents.all()
 
-bot = Bot(command_prefix = '/', intents = intents)
-
+bot = Bot(command_prefix='/', intents = intents)
 
 bot.load_extension('cogs.commands.send_role_embed')
 bot.load_extension('cogs.event.add_role')
@@ -22,6 +21,7 @@ bot.load_extension('cogs.commands.send_embed')
 bot.load_extension('cogs.commands.send')
 bot.load_extension('cogs.warns.add_warn')
 bot.load_extension('cogs.event.message_to_embed')
+bot.load_extension('cogs.event.user_join_guild')
 
 
 token = os.environ.get('TOKEN')
--- owner_func/test.py
+++ owner_func/test.py
@@ -1,42 +1,33 @@
 import logging
 
-import config
 import discord
-import psycopg2
 from discord.ext import commands
 from discord.ext.commands.context import Context
-
+from config import CHANNEL_RULES, CHANNEL_GREETING, CNANNEL_ROLES
 
 class TestOwner(commands.Cog):
-    username = config.USER
-    host = config.HOST
-    database = config.DATABASE
-    password = config.PASSWORD
-    port = 5432
-
     def __init__(self, bot: commands.Bot):
         super().__init__()
         self.bot = bot
+        self.channel_rules = self.bot.get_channel(CHANNEL_RULES)
+        self.channel_greeting = self.bot.get_channel(CHANNEL_GREETING)
+        self.cnannel_roles = self.bot.get_channel(CNANNEL_ROLES)
 
     @commands.command(name = 'test')
     @commands.is_owner()
     async def _test_owner(self, ctx: Context):
         try:
-            conn = psycopg2.connect(
-                user = self.username,
-                host = self.host,
-                database = self.database,
-                password = self.password,
-                port = self.port
-            )
-
-            cursor = conn.cursor()
-            
-            cursor.execute("SELECT message_id FROM public.general WHERE channel_id = 1")
-            data = cursor.fetchone()
-            conn.commit()
-
-            await ctx.send(f'{data[0][0]}\n{data[0][1]}')
+            emb = (discord.Embed(
+                title = f'Вітаємо вас на сервері проекту {ctx.guild.name}!',
+                description = 'Головна мета IT-camp це зробити комфортне місце для вивчення різних напрямів ІТ-технологій в професійному середовищі з неймовірною атмосферою та крутою командою💥\n' \
+                            f'Ознайометесь з правилами проекту в каналі {self.channel_rules.mention}.\n'
+                            f'Також можете вибрати роль в каналі {self.cnannel_roles.mention}, яка найбільше підходить до вашого виду діяльності.',
+                timestamp = ctx.message.created_at
+            ).set_footer(
+                text = f'{ctx.author.id} | Приємно провести час на нашому проекті',
+                icon_url = ctx.author.icon_url
+            ))
+            await self.channel_greeting.send(f'{ctx.author.mention}', embed = emb)
         except Exception as e:
             logging.exception(e)
 
